77b9a6f534c634856277019dd4c0eb97c05ac6ba
vinog <vinogradskiy.n@gmail.com>
2024-07-26 12:15:47 +0200

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,14 @@
-// // PROD
-// const path = require("path");
-// const webpack = require("webpack");
-// const TerserPlugin = require("terser-webpack-plugin");
-
-// module.exports = {
-//   entry: {
-//     content: path.join(__dirname, "src", "extension", "content.ts"),
-//     background: path.join(__dirname, "src", "extension", "background.ts"),
-//   },
-//   mode: "production",
-//   module: {
-//     rules: [
-//       {
-//         test: /\.ts$/,
-//         use: [
-//           {
-//             loader: "ts-loader",
-//             options: {
-//               transpileOnly: true,
-//               configFile: "tsconfig.extension.json",
-//             },
-//           },
-//         ],
-//         exclude: /node_modules/,
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: [".ts", ".js"],
-//     modules: [path.resolve(__dirname, "src", "extension"), "node_modules"],
-//   },
-//   output: {
-//     filename: "[name].js",
-//     path: path.resolve(__dirname, "build"),
-//   },
-//   optimization: {
-//     minimize: true,
-//     concatenateModules: true,
-//     minimizer: [
-//       new TerserPlugin({
-//         terserOptions: {
-//           compress: {
-//             drop_console: true,
-//           },
-//           mangle: true,
-//           format: {
-//             comments: false,
-//           },
-//         },
-//         extractComments: false,
-//       }),
-//     ],
-//   },
-//   plugins: [
-//     new webpack.DefinePlugin({
-//       "process.env.REACT_APP_BASE_API_URL": JSON.stringify(
-//         "${process.env.REACT_APP_BASE_API_URL}"
-//       ),
-//     }),
-//     new webpack.optimize.LimitChunkCountPlugin({
-//       maxChunks: 1,
-//     }),
-//   ],
-// };
-
-//  DEV
+// PROD
 const path = require("path");
 const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
     content: path.join(__dirname, "src", "extension", "content.ts"),
     background: path.join(__dirname, "src", "extension", "background.ts"),
   },
-  mode: "development", // Changed from "production" to "development"
+  mode: "production",
   module: {
     rules: [
       {
@@ -100,8 +35,22 @@ module.exports = {
     path: path.resolve(__dirname, "build"),
   },
   optimization: {
-    minimize: false, // Disable minimization
+    minimize: true,
     concatenateModules: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+          mangle: true,
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
@@ -113,5 +62,56 @@ module.exports = {
       maxChunks: 1,
     }),
   ],
-  devtool: "source-map", // Add source map for better debugging
 };
+
+// //  DEV
+// const path = require("path");
+// const webpack = require("webpack");
+
+// module.exports = {
+//   entry: {
+//     content: path.join(__dirname, "src", "extension", "content.ts"),
+//     background: path.join(__dirname, "src", "extension", "background.ts"),
+//   },
+//   mode: "development", // Changed from "production" to "development"
+//   module: {
+//     rules: [
+//       {
+//         test: /\.ts$/,
+//         use: [
+//           {
+//             loader: "ts-loader",
+//             options: {
+//               transpileOnly: true,
+//               configFile: "tsconfig.extension.json",
+//             },
+//           },
+//         ],
+//         exclude: /node_modules/,
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: [".ts", ".js"],
+//     modules: [path.resolve(__dirname, "src", "extension"), "node_modules"],
+//   },
+//   output: {
+//     filename: "[name].js",
+//     path: path.resolve(__dirname, "build"),
+//   },
+//   optimization: {
+//     minimize: false, // Disable minimization
+//     concatenateModules: true,
+//   },
+//   plugins: [
+//     new webpack.DefinePlugin({
+//       "process.env.REACT_APP_BASE_API_URL": JSON.stringify(
+//         "${process.env.REACT_APP_BASE_API_URL}"
+//       ),
+//     }),
+//     new webpack.optimize.LimitChunkCountPlugin({
+//       maxChunks: 1,
+//     }),
+//   ],
+//   devtool: "source-map", // Add source map for better debugging
+// };

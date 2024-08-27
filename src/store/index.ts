ae00d4d14fed74e55ac9ade8470a08c350a07aab
vinog <vinogradskiy.n@gmail.com>
2024-08-27 18:29:37 +0200

    updates

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieReducer from "@/store/movieSlice";
 import authReducer from "@/store/authSlice";
-import languageSlice from "./languageSlice";
+import languageReducer from "./languageSlice";
 
 export const store = configureStore({
   reducer: {
     movie: movieReducer,
-    language: languageSlice,
+    language: languageReducer,
     auth: authReducer,
   },
 });
